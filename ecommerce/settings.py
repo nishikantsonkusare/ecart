8b51d57c67f7b9567c53ae5695a3a85e4a9a65d1
Nishikant Sonkusare <nish.so007@gmail.com>
2021-07-05 11:41:51 +0530

    whitenoise static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'ecommerce.urls'
 
 TEMPLATES = [
